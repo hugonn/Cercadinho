135fd163b4959c7ae8dc026076af5d8e7c3cf6d8
Hugo Neto <hugo.neto@contractor-mc1.com>
2022-06-10 08:53:22 -0300

    up new project

---
+++ RDToBigQuery/RDToBigQueryFunction/Function.cs
@@ -0,0 +1,48 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.BigQuery.V2;
+using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RDToBigQueryFunction
+{
+    public class Function : IHttpFunction
+    {
+        private string url = "{rdStation_URL}";
+        private HttpClient _httpClient = new HttpClient();
+        private string _projectId = "{project_id}";
+
+        public async Task HandleAsync(HttpContext context)
+        {
+            await InsertJsonIntoBigQuery();
+        }
+
+        public async Task InsertJsonIntoBigQuery()
+        {
+            var credential = GoogleCredential.GetApplicationDefault();
+            BigQueryClient client = BigQueryClient.Create(_projectId);
+            BigQueryDataset dataset = client.GetOrCreateDataset("DataSet_Test");
+            BigQueryTable table = dataset.GetOrCreateTable("Table_Test", new TableSchemaBuilder
+            {
+                { "JsonColumn", BigQueryDbType.String },
+            }.Build());
+
+            var json = await GetContactsFromRD();
+
+            table.InsertRow(new BigQueryInsertRow
+            {
+                { "JsonColumn", json},
+            });
+        }
+        public async Task<string> GetContactsFromRD()
+        {
+            var result = await _httpClient.GetStringAsync(url);
+            var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+            return json;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Aplicação que monitora a criação de arquivos .DAT em uma pasta " %HOMEPATH%/
 Aplicação que faz o envio de mensagens via RabbitMQ
 
 ### Tecnologias:
- - .NET6
+ - .NET 6
  - C#
 
  ### Padrões e Princípios:
@@ -34,3 +34,29 @@ Aplicação que faz upload de arquivo no S3 utilizando o localstack para reprodu
 
  ### Padrões e Princípios:
  - [SOLID](https://blog.cleancoder.com/uncle-bob/2020/10/18/Solid-Relevance.html)
+
+## 4 - Escalonador
+Projeto de faculdade que simula o funcionamento de um escalonador
+
+### Tecnologias:
+ - Java
+
+## 5 - RDtoBigQuery
+Função hospedada no Google Cloud que busca dados da  API do RD Station e envia para o Big Query
+
+### Tecnologias:
+ - .NET 3.1
+ - C#
+ - Google Cloud Functions Framework
+ - Google Cloud BigQuery V2
+
+## 6 - RabbitMQSender
+Aplicação faz autenticação 2.0 na API do RD Station e retorna os contatos da api.
+
+### Tecnologias:
+ - .NET 6
+ - C#
+
+ ### Padrões e Princípios:
+ - [Ports and Adapter Pattern](https://alistair.cockburn.us/hexagonal-architecture/)
+ - [Dependency Injection](https://stackify.com/dependency-injection/)
